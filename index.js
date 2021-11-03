42b405acb9ee9d2001553808183541b5168d5541
khalia <khaliaparris01@gmail.com>
2021-11-03 14:54:29 +0000

    finished questions

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 //write your code here
+function reverseString(string){
+    let reverse = ""
+    for (let i = string.length -1; i >= 0; i--){
+        reverse += string[i]
+    }
+    return reverse
+}
+console.log(reverseString("hola"))
